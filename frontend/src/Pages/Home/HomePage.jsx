1eeeadc309f369b8e0f8f2c8362a40cf58fca338
RahulkrWD <rahulkmrgaya21@gmail.com>
2025-07-08 10:34:11 +0530

    frontend and bacend setup

---
+++ frontend/src/Pages/Home/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../../Components/Layout/Layout";
+
+const HomePage = () => {
+    return (
+        <Layout>
+            <h3>Home Page</h3>
+        </Layout>
+    )
+}
+export default HomePage
